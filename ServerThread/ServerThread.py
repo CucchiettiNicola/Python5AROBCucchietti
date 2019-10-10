f0387ecd3f7b3fe0b890287aa5f6a9208898e18e
CucchiettiNicola <nicola.cucchietti@itiscuneo.eu>
2019-10-10 11:20:37 +0200

    Update ServerThread.py

--- ServerThread/ServerThread.py
+++ ServerThread/ServerThread.py
@@ -2,7 +2,7 @@ import socket
 from threading import Thread
 
 BUFFSIZE = 4096
-SERVER_IP = "192.168.10.96"
+SERVER_IP = "192.168.0.106"
 SERVER_PORT = 1234
 
 class ClientThread(Thread):
@@ -14,28 +14,37 @@ class ClientThread(Thread):
         print(f"New thread started for {client_ip}, {port}")
 
     def run(self):
-        global kill
         while True:
-            data = self.conn.recv(BUFFSIZE)
+            try:
+                data = self.conn.recv(BUFFSIZE)
+            except:
+                print("")
             print(f"Server received data: {data.decode()} from {self.client_ip}, {self.client_port}")
-            if data.decode().lower == "exit":
+            if data.decode() == "exit":
                 print("process killed")
-                self.conn.send("exit".encode())
-                for t in listOfThreads:
-                    if Thread.get_ident() != t:
-                        t.join()
+                if listOfConnect != []:
+                    for c in listOfConnect:
+                        if self.conn != c:
+                            c.close()
+                if listOfThreads != []:
+                    for t in listOfThreads:
+                        if t.ident != self.ident:
+                            t.join()
                 break
-            self.conn.send("RECEIVED".encode())
+            else:
+                self.conn.send("RECEIVED".encode())
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((SERVER_IP, SERVER_PORT))
 s.listen(5)
 listOfThreads = []
+listOfConnect = []
 print("Multithreaded Python server: Waiting for connection from TCP client")
 
 while True:
     (conn, (ip,port)) = s.accept()
     newthread = ClientThread(ip,port,conn)
     newthread.start()
+    listOfConnect.append(conn)
     listOfThreads.append(newthread)
